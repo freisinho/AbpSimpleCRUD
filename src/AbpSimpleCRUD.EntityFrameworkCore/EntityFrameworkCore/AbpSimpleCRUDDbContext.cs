e382bfb1f73c7ac98486bc3962322ce66fd1c504
Fabricio Damazio <fabridamazio@hotmail.com>
2018-01-08 23:53:39 -0200

    Add Client DbSet

--- src/AbpSimpleCRUD.EntityFrameworkCore/EntityFrameworkCore/AbpSimpleCRUDDbContext.cs
+++ src/AbpSimpleCRUD.EntityFrameworkCore/EntityFrameworkCore/AbpSimpleCRUDDbContext.cs
@@ -3,13 +3,16 @@ using Abp.Zero.EntityFrameworkCore;
 using AbpSimpleCRUD.Authorization.Roles;
 using AbpSimpleCRUD.Authorization.Users;
 using AbpSimpleCRUD.MultiTenancy;
+using AbpSimpleCRUD.Models.Clients;
 
 namespace AbpSimpleCRUD.EntityFrameworkCore
 {
     public class AbpSimpleCRUDDbContext : AbpZeroDbContext<Tenant, Role, User, AbpSimpleCRUDDbContext>
     {
         /* Define a DbSet for each entity of the application */
-        
+
+        public DbSet<Client> Clients { get; set; }
+
         public AbpSimpleCRUDDbContext(DbContextOptions<AbpSimpleCRUDDbContext> options)
             : base(options)
         {
