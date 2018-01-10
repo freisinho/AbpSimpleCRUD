7f2e1c5bb81e8a91422ae777be22485e58ef0bab
Fabricio Damazio <fabridamazio@hotmail.com>
2018-01-10 20:21:49 -0200

    Remove Client.cs unnecessary usings

--- src/AbpSimpleCRUD.Core/Models/Clients/Client.cs
+++ src/AbpSimpleCRUD.Core/Models/Clients/Client.cs
@@ -1,9 +1,7 @@
 ﻿using Abp.Domain.Entities.Auditing;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 
 namespace AbpSimpleCRUD.Models.Clients
 {
