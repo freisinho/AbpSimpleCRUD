a983d0f89d2278f6322f4d1737d611a53ecbd1ff
Fabricio Damazio <fabridamazio@hotmail.com>
2018-01-10 21:25:10 -0200

    Add Client Domain service

---
+++ src/AbpSimpleCRUD.Core/Domain/Clients/ClientManager.cs
@@ -0,0 +1,36 @@
+﻿using Abp.Domain.Repositories;
+using Abp.Domain.Services;
+using System.Threading.Tasks;
+
+namespace AbpSimpleCRUD.Domain.Clients
+{
+    public class ClientManager : IDomainService, IClientManager
+    {
+        private IRepository<Client, long> _clientRepository;
+
+        public ClientManager(IRepository<Client, long> clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        public async Task<long> Create(Client client)
+        {
+            return await _clientRepository.InsertAndGetIdAsync(client);
+        }
+
+        public async Task<Client> Update(Client client)
+        {
+            return await _clientRepository.UpdateAsync(client);
+        }
+
+        public async Task Delete(long id)
+        {
+            await _clientRepository.DeleteAsync(id);
+        }
+
+        public async Task<Client> GetById(long id)
+        {
+            return await _clientRepository.GetAsync(id);
+        }
+    }
+}
---
+++ src/AbpSimpleCRUD.Core/Domain/Clients/IClientManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbpSimpleCRUD.Domain.Clients
+{
+    public interface IClientManager
+    {
+        Task<long> Create(Client client);
+        Task<Client> Update(Client client);
+        Task Delete(long id);
+        Task<Client> GetById(long id);
+    }
+}
