94c425494dc09e4a73b1c611e35959a5d1068935
Fabricio Damazio <fabridamazio@hotmail.com>
2018-01-11 23:29:57 -0200

    Add custom map to CreateClient method

--- src/AbpSimpleCRUD.Application/AbpSimpleCRUDApplicationModule.cs
+++ src/AbpSimpleCRUD.Application/AbpSimpleCRUDApplicationModule.cs
@@ -2,6 +2,8 @@
 using Abp.Modules;
 using Abp.Reflection.Extensions;
 using AbpSimpleCRUD.Authorization;
+using AbpSimpleCRUD.Clients.Dtos;
+using AbpSimpleCRUD.Domain.Clients;
 
 namespace AbpSimpleCRUD
 {
@@ -13,6 +15,13 @@ namespace AbpSimpleCRUD
         public override void PreInitialize()
         {
             Configuration.Authorization.Providers.Add<AbpSimpleCRUDAuthorizationProvider>();
+
+            Configuration.Modules.AbpAutoMapper().Configurators.Add(config =>
+            {
+                config.CreateMap<CreateClientInput, Client>()
+                .ConstructUsing(x => new Client(x.FirstName, x.LastName))
+                .AfterMap((src, dest) => dest.AddAge(src.Age));
+            });
         }
 
         public override void Initialize()
