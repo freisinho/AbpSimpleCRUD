20ca38d87fab0c1d41088c4fd3172cd1fd2c7e97
Fabricio Damazio <fabridamazio@hotmail.com>
2018-01-15 22:03:14 -0200

    Add Client index view

---
+++ src/AbpSimpleCRUD.Web.Mvc/Controllers/ClientsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.AspNetCore.Mvc.Controllers;
+using AbpSimpleCRUD.Clients;
+using AbpSimpleCRUD.Web.Models.Clients;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AbpSimpleCRUD.Web.Mvc.Controllers
+{
+    public class ClientsController : AbpController
+    {
+        private readonly IClientAppService _clientAppService;
+
+        public ClientsController(IClientAppService clientAppService)
+        {
+            _clientAppService = clientAppService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var clients = await _clientAppService.GetAllClients();
+            var model = new ClientListViewModel
+            {
+                Clients = clients.Clients
+            };
+            return View(model);
+        }
+    }
+}
---
+++ src/AbpSimpleCRUD.Web.Mvc/Models/Clients/ClientListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AbpSimpleCRUD.Clients.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbpSimpleCRUD.Web.Models.Clients
+{
+    public class ClientListViewModel
+    {
+        public IReadOnlyList<GetAllClientsItem> Clients { get; set; }
+    }
+}
